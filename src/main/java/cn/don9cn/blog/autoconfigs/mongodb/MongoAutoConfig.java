cbfcff1dba911725f38ca988425fb24dee34ee16
don9 <864426487@qq.com>
2017-10-30 00:00:30 +0800

    mongod配置优化

---
+++ src/main/java/cn/don9cn/blog/autoconfigs/mongodb/MongoAutoConfig.java
@@ -0,0 +1,14 @@
+package cn.don9cn.blog.autoconfigs.mongodb;
+
+/**
+ * @Author: liuxindong
+ * @Description:
+ * @Create: don9 2017/10/29
+ * @Modify:
+ */
+public class MongoAutoConfig {
+
+
+
+
+}
