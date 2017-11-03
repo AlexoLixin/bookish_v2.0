0b697de87aa956397df4bad81cdfee67b3a2e168
don9 <864426487@qq.com>
2017-11-03 14:07:42 +0800

    引入guava

---
+++ src/main/java/cn/don9cn/blog/test/guava/ObjectsTest.java
@@ -0,0 +1,23 @@
+package cn.don9cn.blog.test.guava;
+
+
+import com.google.common.base.Objects;
+import com.google.common.base.Throwables;
+
+/**
+ * @author Don9
+ * @create 2017-11-03-9:34
+ **/
+public class ObjectsTest {
+
+    public static void main(String[] args) {
+       try {
+           int i = 5/0;
+       }catch (Exception e){
+           String cause = Throwables.getStackTraceAsString(e);
+           System.out.println(cause);
+       }
+    }
+
+
+}
